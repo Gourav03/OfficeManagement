b768bf50beff29c205b1b57f1a571aef857346a8
Gourav03 <gsen@strivepool.com>
2016-11-01 19:04:34 +0530

    application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!, except: :index
-  after_action :check_date_of_birth
+ 
 
-  private 
-
-  def check_date_of_birth
-  	if (user_signed_in?)  		 
-  		 User.pluck(:date_of_birth)  
-
-  	end
-  end
-
-
+ 
 end
   
