044fe0728cb205fa2014a3f91945da84baf38977
Gourav03 <gsen@strivepool.com>
2016-11-01 18:16:25 +0530

    for host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,3 +84,18 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
+  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.action_mailer.default_url_options = { host: 'https://strivepool-office-management.herokuapp.com' }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+
+config.action_mailer.smtp_settings = {
+ :address              => "smtp.gmail.com",
+ :port                 => 587,
+ :user_name            => 'gourav031192@gmail.com',
+ :password             => '7089047127',
+ :authentication       => "login",
+ :enable_starttls_auto => true
+}
+end
+
