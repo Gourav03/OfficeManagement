b699e319f897e0a730edf81911e7bc95113e59c2
Gourav03 <gsen@strivepool.com>
2016-11-02 11:28:21 +0530

    commit for changes in galleries controller

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -1,6 +1,5 @@
 class GalleriesController < ApplicationController
-  # GET /galleries
-  # GET /galleries.json
+  
   def index
     @galleries = Gallery.all
 
@@ -10,51 +9,36 @@ class GalleriesController < ApplicationController
     end
   end
 
-  # GET /galleries/1
-  # GET /galleries/1.json
   def show
     @gallery  = Gallery.find(params[:id])
     @pictures = @gallery.pictures
-
     respond_to do |format|
-      format.html # show.html.erb
+      format.html 
       format.json { render json: @gallery }
     end
   end
 
-  # GET /galleries/new
-  # GET /galleries/new.json
   def new
     @gallery = Gallery.new
-
     respond_to do |format|
-      format.html # new.html.erb
+      format.html 
       format.json { render json: @gallery }
     end
   end
-
-  # GET /galleries/1/edit
+  
   def edit    
     @gallery = Gallery.find(params[:id])
   end
-
-  # POST /galleries
-  # POST /galleries.json
+ 
   def create
     @gallery = Gallery.new(gallery_params)
-
-    respond_to do |format|
-      
+    respond_to do |format|      
       if @gallery.save
-
-        if params[:images]
-          # The magic is here ;)
+        if params[:images]          
           params[:images].each { |image|
-            @gallery.pictures.create(image: image)
-            
-          }
+                                  @gallery.pictures.create(image: image)            
+                                 }
         end
-
         format.html { redirect_to @gallery, notice: 'Gallery was successfully created.' }
         format.json { render json: @gallery, status: :created, location: @gallery }
       else
@@ -64,18 +48,14 @@ class GalleriesController < ApplicationController
     end
   end
 
-  # PUT /galleries/1
-  # PUT /galleries/1.json
   def update
     @gallery = Gallery.find(params[:id])
-
     respond_to do |format|
       if @gallery.update_attributes(gallery_params)
         if params[:images]
-          # The magic is here ;)
-          params[:images].each { |image|
-            @gallery.pictures.create(image: image)
-          }
+            params[:images].each { |image|
+                                    @gallery.pictures.create(image: image)
+                                  }
         end
         format.html { redirect_to @gallery, notice: 'Gallery was successfully updated.' }
         format.json { head :no_content }
@@ -88,12 +68,10 @@ class GalleriesController < ApplicationController
 
 
   def destroy
-
     @gallery = Gallery.find(params[:id])
     @gallery.destroy
-
     respond_to do |format|
-      format.html { redirect_to galleries_url }
+      format.html { redirect_to galleries_path }
       format.json { head :no_content }
     end
   end
