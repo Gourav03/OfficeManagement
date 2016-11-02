ed7c07d5e2d6e4bcad1bf6b84102b666d32d2593
Gourav03 <gsen@strivepool.com>
2016-11-02 14:28:08 +0530

    for database change

--- db/migrate/20161102084543_remove_contact_number_from_profile.rb
+++ db/migrate/20161102084543_remove_contact_number_from_profile.rb
@@ -1,10 +1,10 @@
 class RemoveContactNumberFromProfile < ActiveRecord::Migration[5.0]
   def change
   	remove_column :profiles, :contact_number, :integer
-  	add_coloumn :profiles, :contact_number, :string
+  	add_column :profiles, :contact_number, :string
   	remove_column :records, :date, :datetime
-  	add_coloumn :records, :date, :date
+  	add_column :records, :date, :date
   	remove_column :records, :total_working_hours, :integer
-  	add_coloumn :records, :total_working_hours, :float  	
+  	add_column :records, :total_working_hours, :float  	
   end
 end
