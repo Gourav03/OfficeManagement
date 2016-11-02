e82aaf99c6cd7ed48f0841a2453c1b67a65add99
Gourav03 <gsen@strivepool.com>
2016-11-02 11:00:26 +0530

    commit for changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,12 @@
 class User < ApplicationRecord
-
 	has_one :profile, dependent: :destroy
-  has_one :record
- 
+  has_one :record 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, #:registerable,
        :recoverable, :rememberable, :trackable, :validatable, :confirmable,
         :lockable
- 
-
   after_create :create_profile
-  
 
   def create_profile
   	self.build_profile(name: self.email.split('@')[0])
