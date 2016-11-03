ac55b6f342b54f86664af02d6be50d4c0df2bb81
Gourav03 <gsen@strivepool.com>
2016-11-03 12:09:21 +0530

    user_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,8 @@ class UsersController < ApplicationController
 	end
 
 	def create		
-		user = User.new(user_params)			
-		user.save ? (redirect_to users_path) : (render 'new')		
+		@user = User.new(user_params)				
+		@user.save ? (redirect_to users_path) : (render 'new')		
 	end
 
 	def edit
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
 
 	private
 	def user_params
-		params.require(:user).permit(:email, :password)
+		params.require(:user).permit(:email, :password, :password_confirmation)
 	end
 
 end
